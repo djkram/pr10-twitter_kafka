3e93944358c1953e20b79e95b23bef554ea19ecf
djkram <marc.planaguma@gmail.com>
2013-05-08 09:25:29 +0200

    final 1er dia

--- src/main/java/org/bdigital/TwitterKafkaConsumer.java
+++ src/main/java/org/bdigital/TwitterKafkaConsumer.java
@@ -1,6 +1,17 @@
 package org.bdigital;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaMessageStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+import kafka.message.Message;
 
 public class TwitterKafkaConsumer {
 
@@ -10,12 +21,36 @@ public class TwitterKafkaConsumer {
     public static void main(String[] args) {
 
 	Properties props = new Properties();
-	
-	//zookeper
+
+	// zookeper
 	props.put("zk.connect", "localhost:2181");
 	props.put("zk.connectiontimeout.ms", "100000");
-	props.put("groupid", "tweetsGroup");
-	
+	props.put("groupid", "tweetsGroup"); // balanceja entre tots els
+					     // consumers d'aquest id
+
+	// Consumer
+	String topicName = "realtimetweets";
+
+	ConsumerConfig consumerConfig = new ConsumerConfig(props);
+	ConsumerConnector consumerConnector = kafka.consumer.Consumer
+		.createJavaConsumerConnector(consumerConfig);
+
+	Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+	topicCountMap.put(topicName, new Integer(1));
+	Map<String, List<KafkaMessageStream<Message>>> consumerMap = consumerConnector
+		.createMessageStreams(topicCountMap);
+	KafkaMessageStream<Message> stream = consumerMap.get(topicName).get(0);
+
+	ConsumerIterator<Message> it = stream.iterator();
+	while (it.hasNext() == true) {
+	    Message message = it.next();
+	    ByteBuffer buffer = message.payload();
+	    byte[] data = new byte[buffer.remaining()];
+	    buffer.get(data);
+	    String s = new String(data, Charset.forName("UTF-8"));
+	    System.out.println("MENSAJE RECIBIDO: " + s);
+	}
+
     }
 
 }
